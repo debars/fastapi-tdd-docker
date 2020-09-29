8fa83102c3a05cdf16f8248e75f8e908a6c47875
Alan Schmitz <alan.schmitz88@gmail.com>
2020-09-29 15:05:03 -0600

    working on config, functional log file

--- project/app/config.py
+++ project/app/config.py
@@ -4,21 +4,41 @@ import logging
 import os
 
 
-log = logging.getLogger(__name__)
-
-
 class Config:
-    def __init__(self, environment: str, testing: bool):
+    def __init__(self, environment: str, testing: bool, loglevel: str):
         self.environment = environment
         self.testing = testing
+        levels = {
+            "DEBUG": logging.DEBUG,
+            "INFO": logging.INFO,
+            "WARNING": logging.WARNING,
+            "ERROR": logging.ERROR,
+            "CRITICAL": logging.CRITICAL,
+        }
+        self.loglevel = levels[loglevel]
 
     @classmethod
     def from_environ(cls):
         return cls(
             os.environ["ENVIRONMENT"],
             os.environ["TESTING"],
+            os.environ["LOGLEVEL"],
         )
 
-
-log.info("Loading config settings from the environment...")
 config = Config.from_environ()
+log = logging.getLogger(__name__)
+log.setLevel(config.loglevel)
+# create console handler and set level to debug
+logpath = os.path.join('.', 'my.log')
+fh = logging.FileHandler(logpath)
+fh.setLevel(config.loglevel)
+
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# add formatter to console handler
+fh.setFormatter(formatter)
+
+# add console handler to logger
+log.addHandler(fh)
+log.info("Loading config settings from the environment...")
