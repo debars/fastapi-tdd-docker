aee578f8c3b02abbf581f0b8b93f0245ef0d7a24
Alan Schmitz <alan.schmitz88@gmail.com>
2020-09-29 15:18:55 -0600

    working cleanup of logger

--- project/app/config.py
+++ project/app/config.py
@@ -5,40 +5,44 @@ import os
 
 
 class Config:
-    def __init__(self, environment: str, testing: bool, loglevel: str):
+    def __init__(self, environment: str, testing: bool):
         self.environment = environment
         self.testing = testing
-        levels = {
-            "DEBUG": logging.DEBUG,
-            "INFO": logging.INFO,
-            "WARNING": logging.WARNING,
-            "ERROR": logging.ERROR,
-            "CRITICAL": logging.CRITICAL,
-        }
-        self.loglevel = levels[loglevel]
 
     @classmethod
     def from_environ(cls):
         return cls(
             os.environ["ENVIRONMENT"],
             os.environ["TESTING"],
-            os.environ["LOGLEVEL"],
         )
 
+    def log(self):
+        return self.log
+
+
+class Logger():
+    def __init__(self, loglevel=logging.DEBUG):
+        self._log = logging.getLogger(__name__)
+        self._log.setLevel(loglevel)
+        # create console handler and set level to debug
+        logpath = os.path.join('.', 'my.log')
+        fh = logging.FileHandler(logpath)
+        fh.setLevel(loglevel)
+
+        # create formatter
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+        # add formatter to console handler
+        fh.setFormatter(formatter)
+
+        # add console handler to logger
+        self._log.addHandler(fh)
+
+    @property
+    def log(self):
+        return self._log
+
+
 config = Config.from_environ()
-log = logging.getLogger(__name__)
-log.setLevel(config.loglevel)
-# create console handler and set level to debug
-logpath = os.path.join('.', 'my.log')
-fh = logging.FileHandler(logpath)
-fh.setLevel(config.loglevel)
-
-# create formatter
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-# add formatter to console handler
-fh.setFormatter(formatter)
-
-# add console handler to logger
-log.addHandler(fh)
-log.info("Loading config settings from the environment...")
+logger = Logger()
+logger.log.info("Loading config settings from the environment...")
