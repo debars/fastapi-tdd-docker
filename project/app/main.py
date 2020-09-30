b5d698de4721228aa7ff93d2bdbfb8e8f2c49bf3
Alan Schmitz <alan.schmitz88@gmail.com>
2020-09-30 10:35:23 -0600

    working tortoise integration

--- project/app/main.py
+++ project/app/main.py
@@ -2,10 +2,20 @@
 
 
 from fastapi import FastAPI
+from tortoise.contrib.fastapi import register_tortoise
 
 from app.api.views import views
+from app.config import config
 
 
 app = FastAPI()
+
+register_tortoise(
+    app,
+    db_url=config.database_url,
+    modules={"models": ["app.api.models"]},
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
 
 app.include_router(views)
